e0c79c827abd0195fdcfaa0d846f0470454d6abb
Enrico Salvucci <enrico.salvucci@studio.unibo.it>
2013-07-26 22:48:36 +0200

    creato il file funzioni.c e sistemato l'header, sono stati dichiarati come parametri delle funzioni dli  gli output di ogni funzione

--- progetto/split_string.c
+++ progetto/split_string.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,7 +7,9 @@
 int main()
 {
 	char *ris=NULL;
-	char str[]= "plus(pow(x,23),pow(x,2))";     //stringa in imput
+	
+	//char str[]= "plus(plus(plus(plus(plus(5,6),x),x),5),x)";     //stringa in imput
+	char str[]= "plus(plus(x,3),plus(33,4))";     //stringa in imput
 	ris=split(str);
 	printf("Risultato =%s\n",ris);
 	return 0;
