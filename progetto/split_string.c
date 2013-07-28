57a4c36a667fbd252affda7975d2e8d09cdc581a
Enrico Salvucci <enrico.salvucci@studio.unibo.it>
2013-07-28 23:38:05 +0200

    Implementato menu e inserimento da file

--- progetto/split_string.c
+++ progetto/split_string.c
@@ -4,56 +4,99 @@
 #include <string.h>
 #include "funzioni.h"
 
+
 int main()
 {
-	char *ris=NULL;
+
+
+	FILE *file_input, *file_output;
+	char *risultato=NULL;
+
+	char stringa[100];
+	int scelta = 0;
+
+	do
+		{
+			printf("\nSi scelga una tra queste opzioni\n");
+			printf("\n1 - Aprire il file funzione.txt che contiene la funzione da derivare\n");
+			printf("\n2 - Inserire la funzione in input tramite tastiera\n");
+			printf("\n3 - Leggere il contenuto del file in output (risultato della derivata)\n");
+			printf("\n4 - Uscire dal programma\n");
+		
+			scanf("%d", &scelta);	
+			while ( getchar() != '\n' );
+
+			switch ( scelta )
+				{
+					case 1 :
+						{
+							printf("Leggo il file funzione.txt\n");
+					
+							file_input = fopen("funzione.txt", "r");
+							if ( file_input == NULL )
+								{
+									printf("Attenzione, il file funzione.txt non è stato trovato!");
+									break;
+								}
+								
+							fscanf(file_input, "%s", stringa);	
+								
+							risultato = split(stringa);
+						
+							fclose(file_input);
+						
+							file_output = fopen("derivata.txt", "w");
+							if ( file_output == NULL )
+								{
+									printf("Attenzione, il file non è stato trovato!");
+									break;
+								}
+						
+							fprintf(file_output, "%s", risultato);
+
+							printf("\nHo scritto il risultato della derivata in derivata.txt");			
+							fclose(file_output);
+					
+							printf("\n\tf(x) = %s\n\n\tDerivata = %s\n\n", stringa, risultato);
+
+							break;
+						}
+					case 2 :
+						{
+							printf("\nSi inserisca una funzione: (max 10000 caratteri)\n");
+							fgets(stringa, 10000, stdin);
+
+							stringa[strlen(stringa)-1] = '\0';	
+									
+							risultato = split(stringa);
+							
+							printf("\n\tf(x) = %s\n\n\tDerivata = %s\n\n", stringa, risultato);
+							
+							break;	
+						}
+					case 3:
+						{
 	
-	//char str[]= "plus(plus(plus(plus(plus(5,6),x),x),5),x)";     //stringa in imput
-//char str[] = "plus(mul(mul(12,pow(x,3)),mul(16,pow(x,2))),mul(cos(x),sin(x)))";
+							file_output = fopen("derivata.txt", "w");
+							if ( file_output == NULL )
+								{
+									printf("\nAttenzione, il file non è stato trovato!\n");
+									break;
+								}
+							
+							if ( risultato == NULL )
+								{
+									printf("\nNon è stata ancora calcolata nessuna derivata!\n\n");
+									break;
+								}
+							fprintf(file_output, "%s", risultato);
+							printf("%s", risultato);
+	
+							fclose(file_output);
+						}
+				}
+			}	
+		while (scelta > 0 || scelta < 4 );
 
-
-//char str[] = "mul(sin(x),mul(cos(x),plus(mul(pow(x,3),plus(x,5)),mul(4,pow(x,2)))))";
-
-
-// 1 Funzione
-//char str[] = "plus(plus(plus(mul(3,pow(x,3)),mul(5,pow(x,2))),mul(12,x)),5)";
-// La prima funzione è corretta
-
-
-
-// 2° funzione
-//char str[] = "plus(mul(mul(12,pow(x,3)),mul(16,pow(x,2))),mul(cos(x),sin(x)))";
-// La seconda funzione è corretta
-
-// 3° Funzione
-//char str[] = "mul(mul(cos(x),plus(mul(sin(x),x),mul(4,pow(x,3)))),mul(sin(x),sot(5,x)))";
-
-// prova
-//char str[] = "plus(mul(3,pow(x,3)),mul(4,pow(x,5)))";
-
-//char str[] = "div(plus(x,4),x)";
-
-char str[] = "plus(pow(2,x),sin(x))";
-
-
-//char str[] = "plus(pow(x,4),sot(x,1))";
-
-/*	Test Set	*/
-/*
-
-
-plus(mul(mul(12,pow(x 3 mul(16,pow(x 2 mul(c(x) s(x)
-
-mul(mul(mul(c(x) plus(mul(4,pow(x 3 mul(x s(x) s(x) - 5 x
-
-mul(s (x) mul(c(x) plus(mul(^ x 3 plus(x 5 mul(4,pow(x 2
-
-plus(mul(plus(c(x) x plus(mul(mul(mul(2 s(x) 4 x c(x) mul(c(x) s(x)
-
-*/
-
-	ris = split(str);
-
-	printf("\n\tf(x) = %s\n\n\tDerivata = %s\n\n", str, ris);
-	return 0;
-}
+		return 0;
+	}
